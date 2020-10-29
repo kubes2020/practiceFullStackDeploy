4046cbf28a207bdccf1870a2d380db64863f81b0
Brian Kubes <briankubes@gmail.com>
2020-10-29 14:23:55 -0700

    setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const server = express()
-
+const path = require('path')
 
 const port = process.env.PORT || 3300
 
@@ -11,6 +11,12 @@ server.use(express.json())
 server.get('/api', (req, res)=> {
     res.json({ message: 'Api is up!'})
 })
+
+server.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.listen(port, ()=> {
     console.log(`\n*** listening on port ${port}***`)
